1aa7d434b3847b94cb0a139f9f673cb28b2edc79
kylejohnston <kylejohnston@users.noreply.github.com>
2015-03-18 22:08:37 -0500

    New work sample; initial commit

---
+++ idea-bucket.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: Idea Bucket Website | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Kyle Johnston Work Sample: Collaborative design and development of an internal idea-sharing website">
+	<meta name="keywords" content="portfolio, kyle johnston, website, design, development">
+	<meta name="author" content="Kyle Johnston">
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+		 header h1 span a:link, 
+		 header h1 span a:hover, 
+		 header h1 span a:active, 
+		 header h1 span a:visited,
+		 .featurette h2 
+		 									{ color: #70992e; }
+		.intro { background-color: #70992e; }
+	</style>
+</head>
+
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="container">
+			<div class="row">
+				<div class="introduction">
+					<h1>Idea Bucket Website</h1>
+					<p>In 2011, I was invited to collaborate with a newly-formed innovation team to design and develop an internal idea-sharing website. The goal of the project was to allow anyone in the company share ideas for new products, features, and services.</p>
+					<p>The website is still in use today and many ideas have advanced to real-world applications.</p>
+					<p><strong>My role:</strong> Website Design, Front End Development</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container marketing">
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span5">
+						<div class="inner">
+							<h2>Homepage</h2>
+							<p>The homepage features the most recent ideas, all sortable by popularity, particular categories, and more.</p>
+							<p>Content can be submitted in three categories:<br>
+							<ul>
+								<li><strong>Add an Idea:</strong> Used to submit ideas for new products, features or services.</li>
+								<li><strong>Share Knowledge:</strong> Used to share links to external news and information about new products, technologies, and more.</li>
+								<li><strong>Submit a Challenge:</strong> Used to gather ideas around distinct challenges.</li>
+							</ul>
+							<p>Once an item is submitted, associates can discuss each idea via comments and vote on ideas, which determines popularity.</p>
+							<p>A small team is responsible for reviewing each idea submitted, then assigning promising ideas to specific teams for further exploration.</p>
+							<p><em>Note: text in the screenshots is blurred due to the proprietary nature of the content.</em></p>
+						</div>
+					</div>
+					<div class="span7">
+						<div class="inner">
+							<img src="img/samples/idea-bucket/homepage.jpg" alt="Idea Bucket Homepage" class="img-border">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="featurette">
+			<div class="container">
+				<div class="row clear">
+					<div class="span5">
+						<div class="inner">
+							<img src="img/samples/idea-bucket/idea-detail.jpg" alt="Idea Bucket - Idea Detail Page" class="img-border">
+						</div>
+					</div>
+					<div class="span7">
+						<div class="inner">
+							<h2>Idea Details &amp; Discussion</h2>
+							<p>Each idea has a dedicated page and can be discussed and debated in detail through threaded comments.</p>
+							<p><em>Note: text in the screenshots is blurred due to the proprietary nature of the content.</em></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php include('includes/footer.php'); ?>
+	</div><!-- /.container -->
+
+	<?php include('includes/foot.php'); ?>
+</body>
+</html>
