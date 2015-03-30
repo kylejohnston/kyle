b16d8d80aa2155b7ac58f485bb3c5954f0db2743
kylejohnston <kylejohnston@users.noreply.github.com>
2015-03-30 12:36:03 -0500

    Initial commit

---
+++ 404.php
@@ -0,0 +1,42 @@
+<?php include('includes/email.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>About Kyle W. Johnston, a Creative Director &amp; Designer in Kansas City</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Oops. I couldn't find the page you were looking for.">
+	<meta name="keywords" content="kyle johnston, creative director, designer, resume, contact, web designer, kansas city, garmin, flow14">
+	<meta name="author" content="Kyle Johnston">
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+		 header h1 span a:link, 
+		 header h1 span a:hover, 
+		 header h1 span a:active, 
+		 header h1 span a:visited, 
+		 h1, h2 
+		 									{ color: #e14526; }
+		.intro { background-color: #e14526; }
+	</style>
+</head>
+
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+	</div>
+	<div class="container items" style="margin-top:2em;">
+		<div class="row">
+			<div class="span12">
+				<h1>Oops.</h1>
+				<p>I couldn't find the page you were looking for. Perhaps you should look at <a href="//kylewjohnston.com/#portfolio">my portfolio</a> or <a href="/about-kyle-johnston/">learn more about me</a>.</p>
+			</div>
+		</div>
+	</div>
+	<div class="container marketing">
+		<?php include('includes/footer.php'); ?>
+	</div>
+
+	<?php include('includes/foot.php'); ?>
+</body>
+</html>
