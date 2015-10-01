7beccae8aa40a2d1f93d95037fe8670fb0dae4dd
Kyle Johnston <kjohnston@Administrators-MacBook-Pro.local>
2015-09-30 23:54:24 -0500

    Updated bio

--- about-kyle-johnston.php
+++ about-kyle-johnston.php
@@ -15,22 +15,20 @@
 		 header h1 span a:active, 
 		 header h1 span a:visited, 
 		 h1, h2 
-		 									{ color: #e14526; }
+		 	 			  { color: #e14526; }
 		.intro { background-color: #e14526; }
 	</style>
 </head>
 
 <body>
-	<div class="bold-border" id="border-top"></div>
-	<div class="bold-border" id="border-right"></div>
-	<div class="bold-border" id="border-left"></div>
-	<div class="bold-border" id="border-bottom"></div>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
 	<div class="intro dark">
 		<?php include('includes/header-nav.php'); ?>
 		<div class="container">
 			<div class="row">
 				<div class="introduction">
-					<h1>A little about me...</h1> <p style="text-align:left;"><img style="float:right; margin:0 0 0 1em; border-radius:5px; opacity:.8;" src="../img/kyle-w-johnston-headshot.png" alt="Kyle W. Johnston, Creative Director">I am currently the Director of Creative at DEG, a rapidly growing digital marketing and advertising agency based in Overland Park, Kansas. I lead a talented creative team dedicated to developing rich and engaging cross-channel digital experiences and drive collaboration across the business in order to develop groundbreaking creative solutions to ever-evolving business challenges for our diverse range of clients.</p>
+					<h1>A little about me...</h1>
+					<p style="text-align:left;"><img style="float:right; margin:0 0 0 1em; border-radius:5px; opacity:.8;" src="../img/kyle-w-johnston-headshot.png" alt="Kyle W. Johnston, Creative Director">I am currently the Director of Creative at DEG, a rapidly growing digital marketing and advertising agency based in Overland Park, Kansas. I lead a talented creative team dedicated to developing rich and engaging cross-channel digital experiences and drive collaboration across the business in order to develop groundbreaking creative solutions to ever-evolving business challenges for our diverse range of clients.</p>
 				</div>
 			</div>
 		</div>
@@ -51,32 +49,6 @@
 				<p>Previous to DEG, I was the Creative Director at Garmin International. In my time at Garmin, I had a front row seat in its explosive growth – from a niche brand known for its early innovations in Marine, Aviation and Outdoor GPS electronics, to its current standing as a global brand, evolving beyond its GPS heritage while maintaining its true North. Similarly, the teams I led there grew – from a small, scrappy band of web designers to a team of over 50, made up of art directors, writers, UX specialists, motion designers and videographers.</p>
 				<p>My time at Garmin also gave me the opportunity to collaborate with other notable brands, including Red Bull, Burton, AT&amp;T, Asus, and others.</p>
 			</div>
-<<<<<<< HEAD
-			<div
-class="span3">                 <h2>Key Skills</h2>                 <ul>
-<li>Art &amp; creative direction</li>                     <li>Web &amp; UX
-design, including front-end design and development of mobile, responsive, and
-desktop websites</li>                     <li>Branding &amp; visual
-identity</li>                     <li>Print design</li> <li>User interface
-design</li>                 </ul> <h2>Speaking</h2> <p><strong><a
-href="http://www.slideshare.net/flow14/herding- cats-53255448">Herding
-Cats</a></strong><br>Federal Reserve Bank - Web Developers, Editors &amp;
-Designers Conference<br>September 2015</p> </div>             <div
-class="span3">                 <h2>Organizations</h2> <p><strong>AAF-
-KC</strong><br>2014&#8212;Present<br>Board of Directors<br>Corporate
-Membership Chair</p>                 <p><strong>AIGA
-KC</strong><br>2011&#8212;2013<br>Web Communications Chair</p>
-<p><strong>Johnson County Community
-College</strong><br>2010&#8212;Present<br>Dean&rsquo;s Advisory Board</p>
-<p><strong>Johnson County Community
-College</strong><br>2006&#8212;Present<br>Interactive Media Advisory Board</p>
-</div>         </div>     </div>     <div class="container">         <div
-class="row">             <h2 class="span12">Say Hi</h2>             <div
-class="span6">                 <a name="contact"></a> <h3>Email Me:</h3> <div
-id="contactWrapper" role="form"> <?php if(isset($hasError)) { //If errors are
-found ?>                     <p class="error">Please check if you've filled
-all the fields with valid information and try again. Thank you.</p> <?php } ?>
-=======
 			<div class="span3">
 				<h2>Key Skills</h2>
 				<ul>
@@ -86,6 +58,8 @@ all the fields with valid information and try again. Thank you.</p> <?php } ?>
 					<li>Print design</li>
 					<li>User interface design</li>
 				</ul>
+				<h2>Speaking</h2>
+				<p><strong><a href="http://www.slideshare.net/flow14/herding-cats-53255448">Herding Cats</a></strong><br>Federal Reserve Bank - Web Developers, Editors &amp; Designers Conference<br>September 2015</p>
 			</div>
 			<div class="span3">
 				<h2>Organizations</h2>
@@ -106,7 +80,6 @@ all the fields with valid information and try again. Thank you.</p> <?php } ?>
 					<?php if(isset($hasError)) { //If errors are found ?>
 					<p class="error">Please check if you've filled all the fields with valid information and try again. Thank you.</p>
 					<?php } ?>
->>>>>>> parent of 1bb509e... Added Speaking section
 					
 					<?php if(isset($emailSent) && $emailSent == true) { //If email is sent ?>
 					<div class="success">
