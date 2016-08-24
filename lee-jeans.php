d0956b4cb7b148b75a34140a03a8e04e68f0455b
Kyle Johnston <flow14@gmail.com>
2016-08-23 21:36:08 -0500

    Initial commit

---
+++ lee-jeans.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: Lee Product Videos | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Videos created for Lee to promote new men's and women's product lines">
+	<meta name="keywords" content="kyle johnston, portfolio, lee, jeans">
+	<meta name="author" content="Kyle Johnston">
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+		 header h1 span a:link, 
+		 header h1 span a:hover, 
+		 header h1 span a:active, 
+		 header h1 span a:visited,
+		 .featurette h2 { color: #c99212; }
+		.intro { background-color: #c99212; }
+	</style>
+</head>
+
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="container">
+			<div class="row">
+				<div class="introduction">
+					<h1>Lee Product Videos</h1>
+					<p>In 2015, Lee delivered a challenge to the team at DEG: concept, shoot, and edit six compelling videos to promote their new men&rsquo;s and women&rsquo;s product lines. <strong>In two weeks.</strong></p>
+					<p>We quickly delivered several concepts and recommended an approach that resulted in two videos that supported Lee&rsquo;s new brand positioning by using themes of movement and energy to tell stories of how the products enable people to move through their day in freedom and style.</p>
+					<p><strong>My role:</strong> Creative Director</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container marketing">
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span6">
+						<div class="inner">
+							<h2>Men&rsquo;s Line</h2>
+							<p>Please <a href="/about-kyle-johnston/#contact">contact me</a> to view this work.</p>
+							<div class="video-container">
+								<iframe src="https://player.vimeo.com/video/179857089?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+					<div class="span6">
+						<div class="inner">
+							<h2>Women&rsquo;s Line</h2>
+							<p>Please <a href="/about-kyle-johnston/#contact">contact me</a> to view this work.</p>
+							<div class="video-container">
+								<iframe src="https://player.vimeo.com/video/179857090?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include('includes/footer.php'); ?>
+	</div><!-- /.container -->
+
+	<?php include('includes/foot.php'); ?>
+</body>
+</html>
