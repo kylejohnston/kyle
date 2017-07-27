f81082a7fae947f19be8ebf4e49bf78337fc95cd
kylejohnston <kylejohnston@users.noreply.github.com>
2017-07-27 10:52:50 -0500

    Removed statcounter code; added hotjar

--- includes/foot.php
+++ includes/foot.php
@@ -4,21 +4,13 @@
 	<script>window.jQuery || document.write('<script src="//flow14.com/js/jquery.js">\x3C/script>')</script>
 	<script src="//kylewjohnston.com/js/jquery.flexslider-min.js"></script>
 	
-	<!-- statcounter analytics -->
-<!-- Start of StatCounter Code for Default Guide -->
-<script type="text/javascript">
-	var sc_project=10324514; 
-	var sc_invisible=1; 
-	var sc_security="42057f08"; 
-	var scJsHost = (("https:" == document.location.protocol) ?
-	"https://secure." : "http://www.");
-	document.write("<sc"+"ript type='text/javascript' src='" +
-	scJsHost+
-	"statcounter.com/counter/counter.js'></"+"script>");
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:576561,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
-<noscript><div class="statcounter"><a title="website
-statistics" href="http://statcounter.com/"
-target="_blank"><img class="statcounter"
-src="http://c.statcounter.com/10324514/0/42057f08/1/"
-alt="website statistics"></a></div></noscript>
-<!-- End of StatCounter Code for Default Guide -->
