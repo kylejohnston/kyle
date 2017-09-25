4687c83c92f656a2154487a4ce97af6341bf6f2a
kylejohnston <kylejohnston@users.noreply.github.com>
2017-09-25 17:42:59 -0500

    Reimplemented tracking code

--- includes/foot.php
+++ includes/foot.php
@@ -1,9 +1,10 @@
-    <!-- Le javascript
-    ================================================== -->
+<!-- Le javascript
+================================================== -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="//flow14.com/js/jquery.js">\x3C/script>')</script>
 	<script src="//kylewjohnston.com/js/jquery.flexslider-min.js"></script>
-	
+
+<!-- hotjar -->	
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -13,4 +14,16 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+<!-- statcounter -->
+<script type="text/javascript">
+var sc_project=10324514; 
+var sc_invisible=1; 
+var sc_security="42057f08"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
 </script>
