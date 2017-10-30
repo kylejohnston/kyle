ff0439514d522c4e01ee3a11360e2e25b9b7893c
Kyle Johnston <flow14@gmail.com>
2017-10-30 17:08:12 -0500

    Update Google Fonts URL

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 	<!-- Le styles -->
 	<link href='//www.kylewjohnston.com/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
 	<link href='//www.kylewjohnston.com/css/custom.css?20170704_0009' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
