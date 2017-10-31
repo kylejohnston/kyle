a655cca1a35ab51b2c0eb4c2ca8035cf6dac1d48
Kyle Johnston <flow14@gmail.com>
2017-10-31 14:28:58 -0500

    Tweaked description

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="span12">
 				<h1><span><a href="/">Kyle Johnston</a></span></h1>
-				<p class="navigation"><span class="epithet">Unflappable Creative Leader &amp; Designer <span>/</span></span> <a href="/#portfolio">Portfolio</a> / <a href="/about-kyle-johnston/">About &#43; Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2017.pdf">Resume</a> (PDF)</p>
+				<p class="navigation"><span class="epithet">Creative Leader &amp; Designer <span>/</span></span> <a href="/#portfolio">Portfolio</a> / <a href="/about-kyle-johnston/">About &#43; Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2017.pdf">Resume</a> (PDF)</p>
 			</div>
 		</div>
 	</div>
