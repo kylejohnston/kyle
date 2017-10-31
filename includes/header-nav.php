d4d6f5048aeef521b659e32db5c9be8cb0304561
Kyle Johnston <flow14@gmail.com>
2017-10-31 14:18:49 -0500

    Updated description, added link to resume

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -2,8 +2,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="span12">
-				<h1><span><a href="//kylewjohnston.com/">Kyle Johnston</a></span></h1>
-				<p class="navigation"><span class="epithet">Creative Director, Designer <span>/</span></span> <a href="//kylewjohnston.com/#portfolio">Portfolio</a> / <a href="/about-kyle-johnston/">About &#43; Contact</a></p>
+				<h1><span><a href="/">Kyle Johnston</a></span></h1>
+				<p class="navigation"><span class="epithet">Unflappable Creative Leader &amp; Designer <span>/</span></span> <a href="/#portfolio">Portfolio</a> / <a href="/about-kyle-johnston/">About &#43; Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2017.pdf">Resume</a> (PDF)</p>
 			</div>
 		</div>
 	</div>
