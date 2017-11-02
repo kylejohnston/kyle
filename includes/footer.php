43fe9a530863d69c99fba9aef5458478c1c977da
Kyle Johnston <flow14@gmail.com>
2017-11-02 12:09:40 -0500

    Nov 2017 enhancements

--- includes/footer.php
+++ includes/footer.php
@@ -4,15 +4,15 @@
 					<div class="span4">
 						<div class="inner">
 							<p>
-								<span class="me">Kyle W. Johnston</span><br>
-								<a href="//www.kylewjohnston.com/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Home']);">Home</a> / <a href="//www.kylewjohnston.com/#portfolio" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Portfolio']);">Portfolio</a> / <a href="//www.kylewjohnston.com/about-kyle-johnston/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'About']);">About &#43; Contact</a>
+								<span class="me">Kyle W. Johnston &#8211; Creative Leader &amp; Designer</span><br>
+								<a href="/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Home']);">Home</a> / <a href="/#portfolio" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Portfolio']);">Portfolio</a> / <a href="/about-kyle-johnston/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'About']);">About Me</a> / <a href="/contact/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Contact']);">Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2017.pdf" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Resume']);">Resume</a> (PDF)
 							</p>
 						</div>
 					</div>
 					<div class="span4">
 						<div class="inner">
 							<p><strong>Contact</strong><br>
-							<span>913 707-7442</span><br><a href="//www.kylewjohnston.com/about-kyle-johnston#contact" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Email']);"><strong>Email</strong></a></p>
+							<span>913 707-7442</span><br><a href="/contact/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Email']);"><strong>Email</strong></a></p>
 						</div>
 					</div>
 					<div class="span4">
