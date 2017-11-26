63458fece07b628bcf75982999d7f84f9dcd73ae
Kyle Johnston <flow14@gmail.com>
2017-11-25 18:54:45 -0600

    Initial commit

---
+++ hallmark-b2b.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: Hallmark B2B Website | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Kyle Johnston Work Sample: Hallmark B2B Website" />
+	<meta name="keywords" content="portfolio, kyle johnston, logo design, business card design, website design" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.kylewjohnston.com/hallmark-b2b" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+		 header h1 span a:link, 
+		 header h1 span a:hover, 
+		 header h1 span a:active, 
+		 header h1 span a:visited,
+		 .featurette h2,.featurette h3 { color: #5A3C8B; }
+		.intro { background-color: #5A3C8B; }
+	</style>
+</head>
+
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="container">
+			<div class="row">
+				<div class="introduction">
+					<h1>Hallmark B2B Microsite</h1>
+					<p>In 2016, a B2B division of Hallmark asked the team at DEG to quickly deliver a new website to help them generate new business leads.</p>
+					<p><strong>My role:</strong> UX Designer, Creative Director</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container marketing">
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span7" data-aos="fade-right">
+						<div class="inner" data-aos-delay="200">
+							<h2>From Simple Beginnings</h2>
+							<p>With a tight timeline and even tighter budget, I stepped in to work on the UX and visual design for this project, freeing my team to focus on other critical initiatives.</p>
+							<p>The team at Hallmark had a simple vision for the website:</p>
+							<ul>
+								<li>Attract new business leads</li>
+								<li>Provide information on Hallmark&rsquo;s card lines</li>
+								<li>Funnel leads to a contact form, powered by Salesforce CRM</li>
+							</ul> 
+							<p>The team at Hallmark provided the visual assets &#8211; videos, photos, and graphics &#8211; and I worked with DEG&rsquo;s UI Team Lead to deliver a clean, responsive website.</p>
+						</div>
+					</div>
+					<div class="span5" data-aos="fade-left">
+						<div class="inner">
+							<div class="caption" data-aos-delay="500">
+								<img src="../img/samples/hmk-greetings/user-flow.full.jpg" alt="User Flow" width="444" height="418">
+								<p>The world's simplest user flow</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="featurette">
+			<div class="container">
+				<div class="row clear">
+					<div class="span2"></div>
+					<div class="span8">
+						<div class="inner">
+							<h2>Wireframes</h2>
+							<p>For each page of the website, I created wireframes at three breakpoints. This helped our partners at Hallmark better understand responsive design and how the pages could adapt to different viewports.</p>
+						</div>
+					</div>
+					<div class="span2"></div>
+				</div>
+				<div class="row clear">
+					<div class="span6">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up">
+								<p>Desktop View</p>
+								<img class="shadow" src="../img/samples/hmk-greetings/wf-home-desktop.920.jpg" alt="Homepage wireframe - desktop view">
+							</div>
+						</div>
+					</div>
+					<div class="span4">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up" data-aos-delay="200">
+								<p>Tablet View</p>
+								<img class="shadow" src="../img/samples/hmk-greetings/wf-home-tablet.full.jpg" alt="Homepage wireframe - tablet view">
+							</div>
+						</div>
+					</div>
+					<div class="span2">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up" data-aos-delay="500">
+								<p>Mobile View</p>
+								<img class="shadow" src="../img/samples/hmk-greetings/wf-home-mobile.full.jpg" alt="Homepage wireframe - mobile view">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clear">
+					<div class="span6">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up">
+								<p>Collections Page</p>
+								<img src="../img/samples/hmk-greetings/wf-product-desktop.920.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+					<div class="span6">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up" data-aos-delay="200">
+								<p>Contact Page</p>
+								<img src="../img/samples/hmk-greetings/wf-contact-desktop.920.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clear nudge-top--lg">
+					<div class="span4">
+						<div class="inner">
+							<h3>Layout Explorations</h3>
+							<p>Rejected ideas for the homepage layout.</p>
+						</div>
+					</div>
+					<div class="span4">
+						<div class="inner">
+							<div data-aos="fade-up">
+								<img src="../img/samples/hmk-greetings/wf-reject-01.600.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+					<div class="span4">
+						<div class="inner">
+							<div data-aos="fade-up" data-aos-delay="200">
+								<img src="../img/samples/hmk-greetings/wf-reject-02.600.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="featurette">
+			<div class="container">
+				<div class="row clear">
+					<div class="span2"></div>
+					<div class="span8">
+						<div class="inner">
+							<h2>A Speedy Delivery</h2>
+							<p>Once the wireframes were approved, the UI lead built the website using <a href="http://patternlab.io">Pattern Lab</a>. With my layouts and content from the Hallmark team, we were able to collaboratively develop a working website to get the signoffs we needed, then deliver frontend code to the Salesforce developers and launch the site in record time.</p>
+						</div>
+					</div>
+					<div class="span2"></div>
+				</div>
+				<div class="row clear">
+					<div class="span6">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up">
+								<p>Homepage</p>
+								<img class="shadow" src="../img/samples/hmk-greetings/home.920.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+					<div class="span6">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up" data-aos-delay="200">
+								<p>Card Line Page</p>
+								<img class="shadow" src="../img/samples/hmk-greetings/card-line.920.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clear">
+					<div class="span3"></div>
+					<div class="span6">
+						<div class="inner">
+							<div class="caption--top" data-aos="fade-up">
+								<p>Contact Page</p>
+								<img class="shadow" src="../img/samples/hmk-greetings/contact.920.jpg" alt="Wireframes" width="1240">
+							</div>
+						</div>
+					</div>
+					<div class="span3"></div>
+				</div>
+			</div>
+		</div>
+		<?php include('includes/footer.php'); ?>
+	</div><!-- /.container -->
+
+	<?php include('includes/foot.php'); ?>
+</body>
+</html>
