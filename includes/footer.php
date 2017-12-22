def69f07a546ad7dd201a29b476a677e361efa43
Kyle Johnston <flow14@gmail.com>
2017-12-22 16:46:18 -0600

    Added Elsewhere links

--- includes/footer.php
+++ includes/footer.php
@@ -7,6 +7,7 @@
 								<span class="me">Kyle W. Johnston &#8211; Creative Leader &amp; Designer</span><br>
 								<a href="/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Home']);">Home</a> / <a href="/#portfolio" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Portfolio']);">Portfolio</a> / <a href="/about-kyle-johnston/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'About']);">About Me</a> / <a href="/contact/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Contact']);">Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2017.pdf" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Resume']);">Resume</a> (PDF)
 							</p>
+							<p><strong>Elsewhere:</strong> <a href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a> / <a href="https://twitter.com/kylejohnston">Twitter</a> / <a href="https://github.com/kylejohnston">GitHub</a> / <a href="https://dribbble.com/kylejohnston">dribbble</a> / <a href="https://unsplash.com/@kylejohnston">Unsplash</a></p>
 						</div>
 					</div>
 					<div class="span4">
