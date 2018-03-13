821be4f3c9a7dff875e3bcf2afb72a67c84755be
Kyle Johnston <flow14@gmail.com>
2018-03-13 14:13:37 -0500

    Added link to current resume

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -4,7 +4,7 @@
 			<div class="span12">
 				<h1><span><a href="/">Kyle Johnston</a></span></h1>
 				<p class="navigation"><span class="epithet">Creative Leader &amp; Designer</span><br>
-				<a href="/#portfolio">Portfolio</a> / <a href="/about-kyle-johnston/">About Me</a> / <a href="/contact/">Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2017.pdf">Resume</a> (PDF)</p>
+				<a href="/#portfolio">Portfolio</a> / <a href="/about-kyle-johnston/">About Me</a> / <a href="/contact/">Contact</a> / <a href="/files/kyle-johnston-creative-leader-designer-2018.pdf">Resume</a> (PDF)</p>
 			</div>
 		</div>
 	</div>
