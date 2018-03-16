6b974df50d5f0e0b22436f57fb15a86cd630919f
Kyle Johnston <flow14@gmail.com>
2018-03-16 14:44:26 -0500

    Initial commit

---
+++ career-path.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: Career Path Framework | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Kyle Johnston Work Sample: Career Path Framework" />
+	<meta name="keywords" content="portfolio, kyle johnston, logo design, business card design, website design" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.kylewjohnston.com/career-path" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+		 h1 { color: #282828; }
+		.intro { background-color: #282828; }
+	</style>
+</head>
+
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+	</div>
+	<div class="container marketing">
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span2"></div>
+					<div class="span8">
+						<div class="inner">
+							<h1>Career Path Framework</h1>
+							<p>While serving as creative director at Garmin in 2016, I undertook a project to develop comprehensive career paths for my team of 50, which included designers, copywriters, UX specialists, videographers, and more.</p>
+							<p><strong>My role:</strong> Creator</p>
+						</div>
+					</div>
+					<div class="span2"></div>
+				</div>
+			</div>
+		</div>
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span2"></div>
+					<div class="span8">
+						<div class="inner">
+							<h2>The Challenge</h2>
+							<p>In exit interviews, the number one piece of feedback we received was people didn't see a way to grow in their roles at the company. While department leaders has a strong understanding of opportunities available to team members, we lacked a clear framework that allowed everyone—team members and managers—to understand how they could progress in their careers with the company and develop the skills needed to achieve their goals.</p>
+						</div>
+					</div>
+					<div class="span2"></div>
+				</div>
+			</div>
+		</div><!-- //featurette -->
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span6" data-aos="fade-up">
+						<div class="inner">
+							<h2>The Approach</h2>
+							<p>I started with the concept of a career lattice. This helped to illustrate a couple of key ideas:
+							<ul>
+								<li>People weren&rsquo;t restricted to one type of role. Provided they met basic qualifications, they had the freedom to take on new roles and develop new skills</li>
+								<li>There was a clear path for both &lsquo;makers&rsquo; and &lsquo;managers&rsquo;</li>
+							</ul>
+							</p>
+						</div>
+					</div>
+					<div class="span6" data-aos="fade-up" data-aos-delay="200">
+						<div class="inner">
+							<div class="caption">
+								<img class="shadow" src="//www.kylewjohnston.com/img/samples/career-path/career-lattice.920.jpg" alt="Career Lattice">
+								<p>The Career Lattice</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clear">
+					<div class="span2"></div>
+					<div class="span8" data-aos="fade-up">
+						<div class="inner">
+							<h3>Defining a Career Path</h3>
+							<p>For each role in the lattice, I then created a career path defining core qualifications, role competencies, duties and responsibilities, and indicators. These details allowed each person to clearly understand what was required of them to progress in their careers. It also helped managers ensure their team members were building relevant skills and experiences.</p>
+							<div class="caption" data-aos="fade-up" data-aos-delay="200">
+								<img class="shadow" src="//www.kylewjohnston.com/img/samples/career-path/maker-career-path-demo.920.jpg" alt="Career Path">
+								<p>&ldquo;Maker&rdquo; Career Path Guidelines</p>
+							</div>
+						</div>
+					</div>
+					<div class="span2"></div>
+				</div>
+			</div>
+		</div><!-- //featurette -->
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span2"></div>
+					<div class="span8">
+						<div class="inner">
+							<h2>Rollout</h2>
+							<p>To roll out the program, I created a presentation and supporting materials to help team members understand the framework, their responsibility in the process, and answer questions.</p>
+						</div>
+					</div>
+					<div class="span2"></div>
+				</div>
+				<div class="row clear nudge-bottom--md">
+					<div class="span4" data-aos="fade-up"><div class="inner"><img class="frame" src="//www.kylewjohnston.com/img/samples/career-path/01-key-what-it-is.600.jpg" alt="Career Path Overview"></div></div>
+					<div class="span4" data-aos="fade-up" data-aos-delay="200"><div class="inner"><img class="frame" src="//www.kylewjohnston.com/img/samples/career-path/02-key-overview.600.jpg" alt="Career Path Overview"></div></div>
+					<div class="span4" data-aos="fade-up" data-aos-delay="500"><div class="inner"><img class="frame" src="//www.kylewjohnston.com/img/samples/career-path/03-key-nomenclature.600.jpg" alt="Career Path Nomenclature"></div></div>
+				</div>
+				<div class="row clear">
+					<div class="span4" data-aos="fade-up"><div class="inner"><img class="frame" src="//www.kylewjohnston.com/img/samples/career-path/04-key-demonstration.600.jpg" alt="Demonstration of Skills"></div></div>
+					<div class="span4" data-aos="fade-up" data-aos-delay="200"><div class="inner"><img class="frame" src="//www.kylewjohnston.com/img/samples/career-path/05-key-mastery.600.jpg" alt="Mastery of Skills"></div></div>
+					<div class="span4"></div>
+				</div>
+			</div>
+		</div><!-- //featurette -->
+		<div class="featurette">
+			<div class="container">
+				<div class="row clear">
+					<div class="span2"></div>
+					<div class="span8">
+						<div class="inner">
+							<h2>What&rsquo;s Next</h2>
+							<p>In addition to introducing this framework at Garmin, I implemented a similar program in my next role at DEG.</p>
+							<p>Based on what I&rsquo;ve learned, I&rsquo;ve developed a consulting program to help other in-house and agency creative teams implement similar career path programs.</p>
+							<p><strong><a href="/contact/">Get in touch</a> if you&rsquo;re interested in getting on the path.</strong></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include('includes/footer.php'); ?>
+	</div><!-- /.container -->
+
+	<?php include('includes/foot.php'); ?>
+</body>
+</html>
