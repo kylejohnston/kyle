e3cf63a54a9a3798871056d51fc8596b2a41a731
Kyle Johnston <flow14@gmail.com>
2018-03-27 17:29:11 -0500

    Initial commit

---
+++ all-projects.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: All Projects | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="The portfolio of Kyle W Johnston, a Creative Director & Designer based in Kansas City." />
+	<meta name="keywords" content="creative director, designer, advertising, web design, logo design, kansas city, garmin, flow14" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.kylewjohnston.com/all-projects/" rel="canonical" />
+
+	<?php include('includes/head.php'); ?>
+
+	<style type="text/css">
+		 h1 { color: #282828; }
+		.intro { background-color: #282828; }
+	</style>
+</head>
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="container">
+			<div class="row">
+			</div>
+		</div>
+	</div>
+
+
+<!-- Thumbnail computed size: 370x185, 2x: 740x370 -->
+	<a name="portfolio"></a>
+	<div class="container items">
+		<h1 class="nudge-top--md">Portfolio: All Projects</h1>
+		<h2 class="nudge-top--lg">&horbar; Agency: DEG</h2>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span6 item">
+				<a href="/ferrellgas-brand-website/"><img src="//www.kylewjohnston.com/img/samples/ferrellgas/thumbnail.2x.jpg" width="740" height="370" alt="Ferrellgas Website Redesign"></a>
+				<div class="inner">
+					<h2><a href="/ferrellgas-brand-website/">Ferrellgas Brand Evolution &amp; Website Redesign</a></h2>
+					<p>Using design thinking to fuel a brand evolution and responsive website design</p>
+					<p><a href="/ferrellgas-brand-website/">Get cozy with the work &#187;</a></p>
+				</div>
+			</div>
+			<div class="span6 item">
+				<a href="/waddell-reed/"><img src="//www.kylewjohnston.com/img/samples/waddell/thumbnail.2x.jpg" width="740" height="370" alt="Ferrellgas Website Redesign"></a>
+				<div class="inner">
+					<h2><a href="/waddell-reed/">Waddell &amp; Reed Website Redesign</a></h2>
+					<p>Investing in a better user experience</p>
+					<p><a href="/waddell-reed/">See how the work paid off &#187;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span6 item">
+				<a href="/amc/"><img src="//www.kylewjohnston.com/img/samples/amc/thumbnail01.2x.jpg" width="740" height="370" alt="New business pitch for AMC Theatres"></a>
+				<div class="inner">
+					<h2><a href="/amc/">AMC New Business Pitch</a></h2>
+					<p>Teamwork made the dream work</p>
+					<p><a href="/amc/">Peek behind the curtain &#187;</a></p>
+				</div>
+			</div>
+			<div class="span6 item">
+				<a href="/amc-coke/"><img src="//www.kylewjohnston.com/img/samples/amc/thumbnail03.2x.jpg" width="740" height="370" alt="Social campaign for AMC Theatres and Coke"></a>
+				<div class="inner">
+					<h2><a href="/amc-coke/">AMC &amp; Coke Campaign</a></h2>
+					<p>How to talk to your teens about Coke &amp; popcorn</p>
+					<p><a href="/amc-coke/">See the refreshing ideas &#187;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span6 item">
+				<a href="/lee-jeans/"><img src="//www.kylewjohnston.com/img/samples/lee-jeans/thumbnail.2x.jpg" width="740" height="370" alt="Lee Product Videos"></a>
+				<div class="inner">
+					<h2><a href="/lee-jeans/">Lee Product Videos</a></h2>
+					<p>Product introduction videos for an iconic brand.</p>
+					<p><a href="/lee-jeans/">See the moving work &#187;</a></p>
+				</div>
+			</div>
+			<div class="span6 item">
+				<a href="/hallmark-b2b/"><img src="//www.kylewjohnston.com/img/samples/hmk-greetings/thumbnail.2x.jpg" width="740" height="370" alt="Hallmark B2B Microsite"></a>
+				<div class="inner">
+					<h2><a href="/hallmark-b2b/">Hallmark Microsite</a></h2>
+					<p>Wireframing &amp; design of a B2B microsite</p>
+					<p><a href="/hallmark-b2b/">See how we delivered &#187;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container items">
+		<h2 class="nudge-top--md">&horbar; In-house: Garmin</h2>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span6 item">
+				<a href="/career-path/"><img src="//www.kylewjohnston.com/img/samples/career-path/thumbnail.2x.jpg" width="740" height="370" alt="Career Path Framework"></a>
+				<div class="inner">
+					<h2><a href="/career-path/">Career Path Framework</a></h2>
+					<p>Designing a creative career path</p>
+					<p><a href="/career-path/">Explore the path &#187;</a></p>
+				</div>
+			</div>
+			<div class="span6 item">
+				<a href="/youtube/"><img src="//www.kylewjohnston.com/img/samples/youtube/thumbnail.2x.jpg" width="740" height="370" alt="YouTube Pre-Roll"></a>
+				<div class="inner">
+					<h2><a href="/youtube/">You-Tube Pre-Roll</a></h2>
+					<p>Pre-roll ads for the launch of Garmin's YouTube channel.<br><strong>Winner:</strong> Gold ADDY Award</p>
+					<p><a href="/youtube/">Roll the video &#187;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span6 item">
+				<a href="/garmin-mobile/"><img src="//www.kylewjohnston.com/img/samples/garmin-mobile/thumbnail.2x.jpg" width="740" height="370" alt="Garmin.com Mobile Website"></a>
+				<div class="inner">
+					<h2><a href="/garmin-mobile/">Garmin.com Mobile Website</a></h2>
+					<p>Wireframing, design, &amp; development of a mobile-optimized Garmin.com</p>
+					<p><a href="/garmin-mobile/">You won&rsquo;t believe the results &#187;</a></p>
+				</div>
+			</div>
+			<div class="span6 item">
+				<a href="/vivofit/"><img src="//www.kylewjohnston.com/img/samples/vivofit/thumbnail.2x.jpg" width="740" height="370" alt="vivofit campaign"></a>
+				<div class="inner">
+					<h2><a href="/vivofit/">v&iacute;vofit</a></h2>
+					<p>Launch campaign &amp; broadcast advertising for Garmin's activity tracker</p>
+					<p><a href="/vivofit/">Step this way to view the work &#187;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span4 item">
+				<a href="/idea-bucket/"><img src="//www.kylewjohnston.com/img/samples/idea-bucket/thumbnail.2x.jpg" width="740" height="370" alt="Idea Bucket Website"></a>
+				<div class="inner">
+					<h2><a href="/idea-bucket/">Idea Bucket Website</a></h2>
+					<p>Design &amp; development of an internal innovation website</p>
+					<p><a href="/idea-bucket/">Explore the work &#187;</a></p>
+				</div>
+			</div>
+			<div class="span4 item">
+				<a href="/holiday/"><img src="//www.kylewjohnston.com/img/samples/holiday/thumbnail.2x.jpg" width="740" height="370" alt="Give-a-Garmin Holiday Campaigns"></a>
+				<div class="inner">
+					<h2><a href="/holiday/">Holiday Campaigns</a></h2>
+					<p>&#x266B; Give-a, Give-a, Give-a, Give-a-Garmin &#x266B;</p>
+					<p><a href="/holiday/">View the work of Christmas past &#187;</a></p>
+				</div>
+			</div>
+			<div class="span4 item">
+				<a href="/nuvifone/"><img src="//www.kylewjohnston.com/img/samples/nuvifone/thumbnail_alt.2x.jpg" width="740" height="370" alt="nuvifone launch campaign"></a>
+				<div class="inner">
+					<h2><a href="/nuvifone/">n&uuml;vifone</a></h2>
+					<p>Brand development &amp; launch materials</p>
+					<p><a href="/nuvifone/">Call up the work samples &#187;</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container items">
+		<h2 class="nudge-top--md">&horbar; Etc.</h2>
+	</div>
+	<div class="container items">
+		<div class="row">
+			<div class="span4 item">
+				<a href="http://www.flow14.com/"><img src="//www.kylewjohnston.com/img/samples/flow14/thumbnail.2x.png" width="370" height="185" alt="flow14 - Independent Projects"></a>
+				<div class="inner">
+					<h2><a href="http://www.flow14.com/">flow14 - Independent Projects</a></h2>
+					<p>Logo design, website design, and more</p>
+					<p><a href="http://www.flow14.com/">See the design works &#187;</a></p>
+				</div>
+			</div>
+			<div class="span4 item">
+				<a href="/iphone-365-project/"><img src="//www.kylewjohnston.com/img/samples/iphone-365/thumb-2017.2x.jpg" width="370" height="185" alt="iPhone 365 Project"></a>
+				<div class="inner">
+					<h2><a href="/iphone-365-project/">Personal Work: 365 Project</a></h2>
+					<p>A photo a day. Every day. Since 2009.
+					<p><a href="/iphone-365-project/">Watch the Videos &#187;</a></p>
+				</div>
+			</div>
+			<div class="span4">&nbsp;</div>
+		</div>
+	</div><!-- Row 1 -->
+	<div class="container items">
+		<div class="row">
+			
+			
+			<div class="span4">&nbsp;</div>
+		</div>
+	</div><!-- Row 3 -->
+	<div class="container marketing">
+		<?php include('includes/footer.php'); ?>
+	</div>
+
+	<?php include('includes/foot.php'); ?>
+
+</body>
+</html>
