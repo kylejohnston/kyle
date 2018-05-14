e7e745671365f7b0c604380a55269e22b712f0f8
Kyle Johnston <flow14@gmail.com>
2018-05-14 14:47:47 -0500

    initial commit

---
+++ rounding-up-undies.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: Rounding Up Undies | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Kyle Johnston Work Sample: Rounding Up Undies" />
+	<meta name="keywords" content="portfolio, kyle johnston, logo design, business card design, website design" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/rounding-up-undies" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+		 header h1 span a:link, 
+		 header h1 span a:hover, 
+		 header h1 span a:active, 
+		 header h1 span a:visited,
+		 h1, h2, h3 { color: #ffa400; }
+		.intro { background-color: #ffa400; }
+		#E9A852
+	</style>
+</head>
+
+<body>
+	<div class="bold-border" id="border-top"></div><div class="bold-border" id="border-right"></div><div class="bold-border" id="border-left"></div><div class="bold-border" id="border-bottom"></div>
+	<div class="intro dark">
+		<?php include('includes/header-nav.php'); ?>
+	</div>
+	<div class="container marketing">
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span2 hidden-mobile"></div>
+					<div class="span8">
+						<div class="inner">
+							<h1>Rounding Up Undies</h1>
+							<p>Rounding Up Undies is a charity that provides hygiene kits to young women in Uganda and Mali. The team at Rounding Up Undies approached me about designing a logo that could be used for their hygiene kit packaging, promotional materials and social presence.</p>
+							<p><strong>My role:</strong> Designer</p>
+						</div>
+					</div>
+					<div class="span2 hidden-mobile"></div>
+				</div>
+			</div>
+		</div>
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span3">
+						<div class="inner">
+							<h2>Research &amp; Sketches</h2>
+							<p>I started by researching Ugandan art and textiles to gain an understanding of dominant colors and patterns present in the culture. At the same time, I began sketching ideas for shapes and type treatments.</p>
+						</div>
+					</div>
+					<div class="span6">
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/research.jpg" width="100%" alt="Brand Research">
+					</div>
+					<div class="span3">
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/sketches.jpg" width="100%" alt="Initial Logo Sketches">
+					</div>
+				</div>
+			</div>
+		</div><!-- //featurette -->
+		<div class="container">
+				<div class="row">
+					<div class="span3 hidden-mobile"></div>
+					<div class="span6 nudge-top--lg">
+						<div class="inner">
+							<h2>Design Exploration</h2>
+							<p>Based on my research and initial sketches, I explored a variety of visual directions and type treatments. At this stage, I started to incorporate hand-drawn shapes and patterns. I also realized that when the underwear shape was 'folded', it resembled the shape of Africa, so it became a key element in my design recommendations.</p>
+						</div>
+					</div>
+					<div class="span3 hidden-mobile"></div>
+				</div>
+				<div class="row">
+					<div class="span12 nudge-bottom--lg">
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/design-exploration.png" width="100%" alt="Logo Design Exploration">
+					</div>
+				</div>
+		</div>
+		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span4" data-aos="fade-up">
+						<div class="inner">
+							<h2>Logo Design Options</h2>
+							<p>After exploring many design ideas, I selected three options and presented them to the Rounding Up Undies team. They quickly gravitated to one design. After a round of refinements, we had a design that everyone loved.</p>
+						</div>
+					</div>
+					<div class="span4 caption--top">
+						<p class="center">Round 1</p>
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/options-round-one.jpg" alt="Logo Options, Round 1">
+					</div>
+					<div class="span4 caption--top">
+						<p>Round 2</p>
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/options-round-two.jpg" alt="Logo Options, Round 2">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="span4" data-aos="fade-up">
+						<div class="inner">
+						<h2>Final Logo Design</h2>
+						<p>After a few additional finishing touches (details matter) &#8212; I delivered the final logo along with brand guidelines that ensure the logo will look great as it's used to support and promote the important mission of Rounding Up Undies.</p>
+						<h3>Impact</h3>
+						<p>Within the first 6 hours of promoting Rounding Up Undies using the new logo, they raised $1,000 – and continue to help women in need to this day.</p>
+						</div>
+					</div>
+					<div class="span4">
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/final-logo.jpg" alt="Final Logo Design">
+					</div>
+					<div class="span4">
+						<img src="//www.kylewjohnston.com/img/samples/rounding-up-undies/rounding-up-undies-guidelines.jpg" alt="Brand Guidelines">
+					</div>
+				</div>
+			</div>
+		</div><!-- //featurette -->
+		<?php include('includes/footer.php'); ?>
+	</div><!-- /.container -->
+
+	<?php include('includes/foot.php'); ?>
+</body>
+</html>
