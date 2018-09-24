86399b3f20679c280a02ab1475114456935340c7
Kyle Johnston <flow14@gmail.com>
2018-09-24 17:39:03 -0500

    Implementing new homepage

--- index.php
+++ index.php
@@ -13,15 +13,11 @@
 	<?php include('includes/head.php'); ?>
 
 	<style type="text/css">
-		header h1 span a:link, 
-		header h1 span a:hover, 
-		header h1 span a:active, 
-		header h1 span a:visited { color: #94bd00; }
-		.intro { background-color: #94bd00; }
-		.welcome h1, .welcome h3 { line-height: 1.5; }
-		.welcome h3 { color: #4E4E4E; }
-		span#hello,
-		h1 span.oomph { border-bottom: 4px solid #94bd00; color: #282828; }
+		.welcome h1 {margin:0}
+		.welcome h1, .welcome h3 {line-height:1.5;}
+		.welcome h3 { color:#141414; }
+		span#hello{color:#141414;}
+		.hello h3 {font-family:"Objectivity Medium";font-weight:500;}
 	</style>
 </head>
 <body>
@@ -32,89 +28,121 @@
 <!-- Thumbnail computed size: 370x185, 2x: 740x370 -->
 	<a name="portfolio"></a>
 	<article class="content">
-	<section class="block">
-		<div class="col-9 welcome spacer-4--bottom">
-			<div class="inner">
+		<section class="block spacer-6">
+			<!-- <div class="col-4 text__center" style="position:relative;">
 				<h1><span class="oomph"><span id="hello">Hello</span>!</span></h1>
-				<h3>I’m a designer and creative director. I focus on brand and interactive design. I’m also a cat person, hobbyist photographer and coder. And these are a few of my favorite things…</h3>
+			</div> -->
+			<div class="col-3 shim"></div>
+			<div class="col-6 welcome">
+				<div class="inner hello">
+					<h1><span id="hello">Hello</span>!</h1>
+					<h3>I’m Kyle Johnston &#8211; a designer and creative director based in Kansas City. I focus on brand and interactive design. I’m also a cat person, hobbyist photographer and coder. And these are a few of my favorite things…</h3>
+				</div>
 			</div>
-		</div>
-	</section>
-	<div class="block">
-		<div class="col-6">
-			<div class="card">
-				<a href="hnc-living-foundation/"><img src="img/samples/hnc-living/thumb-alt.jpg" alt="HNC Living Website Redesign"></a>
+			<div class="col-3 shim"></div>
+		</section>
+		<section class="block spacer-6">
+			<div class="col-1 shim"></div>
+			<div class="col-6">
+				<a href="hnc-living-foundation/"><img class="thumb shadow" src="img/samples/hnc-living/thumb-alt.jpg" alt="HNC Living Website Redesign"></a>
+			</div>
+			<div class="col-5">
 				<div class="inner">
-					<h2><a href="hnc-living-foundation/">HNC Living Website Redesign</a></h2>
+					<p class="spacer-2"><small>Client</small></p>
+					<h2><a href="hnc-living-foundation/">HNC Living Foundation</a></h2>
+					<p class="spacer-2"><small>Project</small></p>
 					<p>Rehabilitating a website for a life-changing nonprofit</p>
 					<p><a href="hnc-living-foundation/">See the transformation &#187;</a></p>
 				</div>
 			</div>
-		</div>
-		<div class="col-6">
-			<div class="card">
-				<a href="ferrellgas-brand-website/"><img src="img/samples/ferrellgas/thumbnail.2x.jpg" alt="Ferrellgas Website Redesign"></a>
+		</section>
+
+		<section class="block spacer-6">
+			<div class="col-1 shim"></div>
+			<div class="col-4 order-2">
 				<div class="inner">
-					<h2><a href="ferrellgas-brand-website/">Ferrellgas Brand Evolution &amp; Website Redesign</a></h2>
+					<p class="spacer-2"><small>Client</small></p>
+					<h2><a href="ferrellgas-brand-website/">Ferrellgas</a></h2>
+					<p class="spacer-2"><small>Project</small></p>
 					<p>Design thinking fueled a brand evolution and website design</p>
 					<p><a href="ferrellgas-brand-website/">Get cozy with the work &#187;</a></p>
 				</div>
 			</div>
-		</div>
-
-		<div class="col-6">
-			<div class="card">
-				<a href="amc/"><img src="/img/samples/amc/thumbnail01.2x.jpg" alt="New business pitch for AMC Theatres"></a>
+			<div class="col-6 order-1">
+				<a href="ferrellgas-brand-website/"><img class="thumb shadow" src="img/samples/ferrellgas/thumbnail.2x.jpg" alt="Ferrellgas Website Redesign"></a>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+		
+		<section class="block spacer-6">
+			<div class="col-1 shim"></div>
+			<div class="col-6">
+				<a href="amc/"><img class="shadow thumb" src="/img/samples/amc/thumbnail01.2x.jpg" alt="New business pitch for AMC Theatres"></a>
+			</div>
+			<div class="col-5">
 				<div class="inner">
-					<h2><a href="amc/">AMC New Business Pitch</a></h2>
+					<p class="spacer-2"><small>Client</small></p>
+					<h2><a href="amc/">AMC Theatres</a></h2>
+					<p class="spacer-2"><small>Project</small></p>
 					<p>Teamwork made the dream work</p>
 					<p><a href="amc/">Peek behind the curtain &#187;</a></p>
 				</div>
 			</div>
-		</div>
-		<div class="col-6">
-			<div class="card">
-				<a href="garmin-mobile/"><img src="img/samples/garmin-mobile/thumbnail.2x.jpg" alt="Garmin.com Mobile Website"></a>
+		</section>
+
+		<section class="block spacer-6">
+			<div class="col-1 shim"></div>
+			<div class="col-4 order-2">
 				<div class="inner">
-					<h2><a href="garmin-mobile/">Garmin.com Mobile Shop</a></h2>
+					<p class="spacer-2"><small>Client</small></p>
+					<h2><a href="garmin-mobile/">Garmin.com</a></h2>
+					<p class="spacer-2"><small>Project</small></p>
 					<p>Fast-moving wireframing, design, and development</p>
 					<p><a href="garmin-mobile/">You won&rsquo;t believe the results &#187;</a></p>
 				</div>
 			</div>
-		</div>
+			<div class="col-6 order-1">
+				<a href="garmin-mobile/"><img class="thumb shadow" src="img/samples/garmin-mobile/thumbnail.2x.jpg" alt="Garmin.com Mobile Website"></a>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
 
-		<div class="col-4">
-			<div class="card">
-				<a href="rounding-up-undies/"><img src="img/samples/rounding-up-undies/thumbnail.jpg" alt="Rounding Up Undies Logo"></a>
+		<section class="block">
+			<div class="col-4 text__center">
 				<div class="inner">
-					<h2><a href="rounding-up-undies/">Rounding Up Undies Logo</a></h2>
+					<a href="rounding-up-undies/"><img class="thumb spacer-3--bottom" src="img/samples/rounding-up-undies/thumbnail.jpg" alt="Rounding Up Undies Logo"></a>
+					<h3><a href="rounding-up-undies/">Rounding Up Undies</a></h3>
 					<p>A distinctive logo design for a unique nonprofit.</p>
 					<p><a href="rounding-up-undies/">Read the round up &#187;</a></p>
 				</div>
 			</div>
-		</div>
-		<div class="col-4">
-			<div class="card">
-				<a href="iphone-365-project/"><img src="img/samples/iphone-365/thumb-2017.2x.jpg" alt="iPhone 365 Project"></a>
+			<div class="col-4 text__center">
 				<div class="inner">
-					<h2><a href="iphone-365-project/">Personal Work: 365 Project</a></h2>
+					<a href="iphone-365-project/"><img class="thumb spacer-3--bottom" src="img/samples/iphone-365/thumb-2017.2x.jpg" alt="iPhone 365 Project"></a>
+					<h3><a href="iphone-365-project/">Personal Work: 365 Project</a></h3>
 					<p>A photo a day. Every day. Since 2009.
 					<p><a href="iphone-365-project/">Watch the Videos &#187;</a></p>
 				</div>
 			</div>
-		</div>
-		<div class="col-4">
-			<div class="card">
-				<a href="http://www.flow14.com/"><img src="img/samples/flow14/thumbnail.png" alt="flow14 - Freelance Projects"></a>
+			<div class="col-4 text__center">
 				<div class="inner">
-					<h2><a href="http://www.flow14.com/">flow14 - Independent Projects</a></h2>
+					<a href="http://www.flow14.com/"><img class="thumb spacer-3--bottom" src="img/samples/flow14/thumbnail.png" alt="flow14 - Freelance Projects"></a>
+					<h3><a href="http://www.flow14.com/">flow14 - Independent Projects</a></h3>
 					<p>Logo design, website design, and more</p>
 					<p><a href="http://www.flow14.com/">See the design works &#187;</a></p>
 				</div>
 			</div>
-		</div>
-		<p class="col-12"><a href="all-projects/"><strong>&horbar;View all projects</strong></a></p>
-	</div><!-- /block -->
+		</section>
+
+		<section class="block spacer-6">
+			<div class="col-12 text__center">
+				<div class="rule"></div>
+				<div class="spacer-4--bottom scale-80"><a href="all-projects/">
+				<svg width="208" height="32" viewBox="0 0 208 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 2C0 0.895431 0.895431 0 2 0H62C63.1046 0 64 0.895431 64 2V30C64 31.1046 63.1046 32 62 32H2C0.895431 32 0 31.1046 0 30V2Z" fill="#E5F5AB"/><path d="M72 2C72 0.895431 72.8954 0 74 0H134C135.105 0 136 0.895431 136 2V30C136 31.1046 135.105 32 134 32H74C72.8954 32 72 31.1046 72 30V2Z" fill="#E5F5AB"/><path d="M144 2C144 0.895431 144.895 0 146 0H206C207.105 0 208 0.895431 208 2V30C208 31.1046 207.105 32 206 32H146C144.895 32 144 31.1046 144 30V2Z" fill="#E5F5AB"/></svg>
+				</a></div>
+				<h3><a href="all-projects/"><strong>View all projects</strong></a></h3>
+			</div>
+		</section><!-- /block -->
 	</article>
 
 	<?php include('includes/footer.php'); ?>
@@ -127,7 +155,6 @@
 			{ text: "Howdy" },
 			{ text: "Aloha" },
 			{ text: "Hiya" },
-			{ text: "Namaste" },
 			{ text: "Konnichi Wa" },
 			{ text: "Bonjour" }
 		];
