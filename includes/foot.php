c4871e1ad5c8927f6f74ab766eee78712b2c3522
Kyle Johnston <flow14@gmail.com>
2018-12-05 14:23:21 -0600

    Removed HotJar code

--- includes/foot.php
+++ includes/foot.php
@@ -1,28 +1,16 @@
 <!-- Fun with JavaScript - - - - - - - - - - -->
-	<script src="//kylewjohnston.com/js/aos.js"></script>
+	<!-- <script src="//kylewjohnston.com/js/aos.js"></script>
     <script type="text/javascript">
       AOS.init({
         duration: 700,
         easing: 'ease-in-out-back'
       });
-    </script>
+    </script> -->
     <!-- <script async src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> -->
     <script>window.jQuery || document.write('<script src="//flow14.com/js/jquery.js">\x3C/script>')</script>
     <script async src="//kylewjohnston.com/js/jquery.flexslider-min.js"></script>
 
 <!-- Analytics - - - - - - - - - - -->
-<!-- hotjar -->	
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:576561,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
 <!-- statcounter -->
 <script type="text/javascript">
 var sc_project=10324514; 
