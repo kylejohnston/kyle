ded3ed63a1f6d054f169395b85813f96aca71718
Kyle Johnston <flow14@gmail.com>
2020-05-25 21:19:47 -0500

    Initial commit. new project

---
+++ work/vmc-group/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio: VMC Group Website Redesign | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Kyle Johnston Work Sample: VMC Group Website Redesign" />
+	<meta name="keywords" content="portfolio, kyle johnston, logo design, app design, user experience" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.kylewjohnston.com/vmc-group/" rel="canonical" />
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+	<style type="text/css">
+		html,article.content::before {background: #111;}
+		.intro h1, .intro h2, .intro p {color:#eee;}
+		h1,h2,h3{color:#111;}
+	</style>
+</head>
+
+<body>
+	<header>
+		<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header-nav.php'); ?>
+	</header>
+
+	<section class="block">
+		<div class="col-1 shim"></div>
+		<div class="col-6 spacer-6 intro">
+			<div class="inner">
+				<h1>VMC Group</h1>
+				<h2>Website Redesign</h2>
+				<p>As a 120-year-old brand, VMC Group is a recognized world leader in the design and manufacture of vibration isolation, seismic control and shock protection products.</p>
+				<p>They wanted to redesign their website to better reflect the growth and maturity of the company and provide a more well-rounded and engaging experience for prospective and existing customers.</p>
+				<p><strong>Credits:</strong><br>
+				<strong>My Role:</strong> IA, UX, Visual Design<br>
+				<strong>InQuest Marketing:</strong> Development, Copywriting, Project and Account Management</p>
+			</div>
+		</div>
+		<div class="col-5 shim"></div>
+	</section>
+	<article class="content">
+		<!-- <section class="block block--narrow spacer-9--top spacer-6--bottom">
+			<div class="pattern__3-6-3">
+				<div class="inner">
+					<h2>Homepage</h2>
+				</div>
+			</div>
+		</section> -->
+
+		<section class="block spacer-9--top">
+			<div class="col-1 shim"></div>
+			<div class="col-7">
+				<div class="inner">
+					<p><strong>Homepage, Desktop</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-homepage.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-homepage-640.jpg">
+							<img class="" src="/img/samples/vmc-group/site-homepage.jpg" alt="VMC Group Homepage" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="inner">
+					<p><strong>Mobile</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-homepage-mobile.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-homepage-mobile.jpg">
+							<img class="" src="/img/samples/vmc-group/site-homepage-mobile.jpg" alt="VMC Group Homepage, mobile" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+
+
+		<section class="block block--narrow spacer-6">
+			<div class="col-3 shim"></div>
+			<div class="col-6">
+				<div class="inner">
+					<h2>Process</h2>
+					<p>Working with the team at InQuest Marketing, I was responsible for the IA, UX, and design of the new website.</p>
+					<h3>Information Architecture</h3>
+					<p>Facing a tight deadline for presenting concepts to the CEO, I started by analyzing the information architecture, and developed a plan for the new IA.</p>
+				</div>
+			</div>
+			<div class="col-3 shim"></div>
+			<div class="col-1 shim"></div>
+			<div class="col-10">
+				<h3 class="text-center">IA, Before</h3>
+				<picture>
+					<source media="(min-width: 1240px)" srcset="/img/samples/vmc-group/sitemap-1440.jpg">
+					<source media="(min-width: 720px)" srcset="/img/samples/vmc-group/sitemap-960.jpg">
+					<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/sitemap-640.jpg">
+					<img class="spacer-7--bottom frame" src="/img/samples/vmc-group/sitemap-2160.jpg" alt="Original IA" width="100%" height="auto" loading="lazy">
+				</picture>
+				<h3 class="text-center">IA, After</h3>
+				<picture>
+					<source media="(min-width: 1240px)" srcset="/img/samples/vmc-group/sitemap-new-1440.jpg">
+					<source media="(min-width: 720px)" srcset="/img/samples/vmc-group/sitemap-new-960.jpg">
+					<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/sitemap-new-640.jpg">
+					<img class="frame" src="/img/samples/vmc-group/sitemap-new-2160.jpg" alt="Original IA" width="100%" height="auto" loading="lazy">
+				</picture>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+		<section class="block block--narrow spacer-6">
+			<div class="col-3 shim"></div>
+			<div class="col-6">
+				<div class="inner">
+					<h3>Concepts and Wireframes</h3>
+					<p>With a solid foundation for the structure of the site, I designed a range of homepage concepts and page wireframes, which I presented to the CEO.</p>
+				</div>
+			</div>
+			<div class="col-3 shim"></div>
+			<div class="col-12">
+				<picture>
+					<source media="(min-width: 1240px)" srcset="/img/samples/vmc-group/mockups-1440.jpg">
+					<source media="(min-width: 720px)" srcset="/img/samples/vmc-group/mockups-960.jpg">
+					<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/mockups-640.jpg">
+					<img class="spacer-7--bottom" src="/img/samples/vmc-group/mockups-2160.jpg" alt="Page Concepts" width="100%" height="auto" loading="lazy">
+				</picture>
+				<picture>
+					<source media="(min-width: 1240px)" srcset="/img/samples/vmc-group/wires-1440.jpg">
+					<source media="(min-width: 720px)" srcset="/img/samples/vmc-group/wires-960.jpg">
+					<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/wires-640.jpg">
+					<img class="" src="/img/samples/vmc-group/wires-2160.jpg" alt="Wireframes" width="100%" height="auto" loading="lazy">
+				</picture>
+			</div>
+		</section>
+		<section class="block spacer-6">
+			<div class="col-3 shim"></div>
+			<div class="col-6">
+				<div class="inner">
+					<h3>Visual Design</h3>
+					<p>Once a visual direction was selected, I designed desktop and mobile-optimized versions for each page of the website. I also designed a style guide which allowed the developer to start building the site while pages were being reviewed and approved by the client.</p>
+				</div>
+			</div>
+			<div class="col-3 shim"></div>
+
+			<div class="col-1 shim"></div>
+			<div class="col-7">
+				<div class="inner">
+					<p><strong>Product Page, Desktop</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-product.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-product-640.jpg">
+							<img class="" src="/img/samples/vmc-group/site-product.jpg" alt="VMC Group Product Page" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="inner">
+					<p><strong>Mobile</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-product-mobile.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-product-mobile.jpg">
+							<img class="" src="/img/samples/vmc-group/site-product-mobile.jpg" alt="VMC Group Product Page, mobile" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+
+		<section class="block spacer-6">
+			<div class="col-1 shim"></div>
+			<div class="col-7">
+				<div class="inner">
+					<p><strong>Services, Desktop</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-services.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-services-640.jpg">
+							<img class="" src="/img/samples/vmc-group/site-services.jpg" alt="VMC Group Services Page" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="inner">
+					<p><strong>Mobile</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-services-mobile.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-services-mobile.jpg">
+							<img class="" src="/img/samples/vmc-group/site-services-mobile.jpg" alt="VMC Group Services Page, mobile" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+
+		<section class="block spacer-6">
+			<div class="col-1 shim"></div>
+			<div class="col-7">
+				<div class="inner">
+					<p><strong>Company, Desktop</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-company.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-company-640.jpg">
+							<img class="" src="/img/samples/vmc-group/site-company.jpg" alt="VMC Group Company Page" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="inner">
+					<p><strong>Mobile</strong></p>
+					<div class="screen-demo rounders shadow-lg">
+						<div></div>
+						<picture>
+							<source media="(min-width: 600px)" srcset="/img/samples/vmc-group/site-company-mobile.jpg">
+							<source media="(min-width: 280px)" srcset="/img/samples/vmc-group/site-company-mobile.jpg">
+							<img class="" src="/img/samples/vmc-group/site-company-mobile.jpg" alt="VMC Group Companyh Page, mobile" loading="lazy">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+	</article>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/foot.php'); ?>
+</body>
+</html>
