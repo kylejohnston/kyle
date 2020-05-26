47feae77e9aa9d32c947ab702a676b9cc9877c28
Kyle Johnston <flow14@gmail.com>
2020-05-25 21:19:15 -0500

    Initial commit

---
+++ resume/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Resume: Kyle W. Johnston, a Creative Director &amp; Designer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="I am a Creative Leader and Designer based in Kansas City." />
+	<meta name="keywords" content="kyle johnston, resume, creative director, designer, resume, contact, web designer, kansas city, garmin, flow14, vista outdoor, deg digital" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.kylewjohnston.com/resume" rel="canonical" />
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+
+<body>
+	<header>
+		<!-- <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header-nav.php'); ?> -->
+	</header>
+	<article class="content">
+		<section class="block spacer-9--top">
+			<div class="col-1 shim"></div>
+			<div class="col-11">
+				<div class="block">
+					<div class="col-7">
+						<div class="inner">
+							<h2>KYLE JOHNSTON</h2>
+                            <p>
+                                <strong>Designer &amp; Creative Director</strong><br>
+                                <a href="mailto:kyle@flow14.com">Email</a><br>
+                                913-707-7442<br>
+                                <strong>Portfolio:</strong> <a href="https://www.kylewjohnston.com">KyleWJohnston.com</a>
+                            </p>
+
+                            <div class="spacer-9">
+                                <h3>KEY SKILLS &amp; TOOLS</h3>
+                                <p><strong>DESIGN:</strong> Web and Print, Branding, User Experience (UX), UI, Wireframing and Prototyping, Design Strategy, Marketing Campaign and Content Development</p>
+                                <p><strong>DEVELOPMENT:</strong> HTML, CSS, JavaScript, PHP, Processing, React, TypeScript</p>
+                                <p><strong>LEADERSHIP:</strong> Creative Direction, Hiring and Team Building, Career Path Development</p>
+                                <p><strong>TOOLS:</strong> Adobe Creative Suite (Illustrator, Photoshop, InDesign), Keynote, Sketch, Figma, InVision, GitHub, WordPress, HubSpot</p>
+                            </div>
+
+                            <div class="spacer-9">
+                                <h3>EXPERIENCE</h3>
+                                <p><strong>FLOW14 <br>
+                                PRINCIPAL DESIGNER</strong><br>
+                                July 2017 – Present</p>
+                                <ul>
+                                    <li>Design memorable digital communications and branding by blending user-centered design and creative strategy using tools like Adobe Creative Suite, Figma, and Sketch.</li>
+                                    <li>Design and development of responsive websites using tools like WordPress as well as custom coded websites using HTML, CSS, and JavaScript.</li>
+                                    <li>Drive audience growth, reach, and impact by consulting on digital creative strategy, including research, presenting findings and recommendations, and creation of assets, design systems, presentations, and training materials.</li>
+                                </ul>
+                            </div>
+
+                            <div class="spacer-9">
+                                <p><strong>STAY ALFRED<br>
+                                SENIOR DESIGNER</strong><br>
+                                April 2019 – October 2019</p>
+                                <ul>
+                                    <li>Concepting and design for a range of digital and print marketing materials using tools like Adobe Creative Suite, Sketch, and Figma.</li>
+                                    <li>Design and frontend development for StayAlfred.com using React and TypeScript.</li>
+                                    <li>Design and development of HubSpot-based emails and landing pages.</li>
+                                </ul>
+                            </div>
+
+                            <div class="spacer-9">
+                                <p><strong>VISTA OUTDOOR<br>
+                                CREATIVE DIRECTOR</strong><br>
+                                October 2016 – June 2017</p>
+                                <ul>
+                                    <li>Designed impactful communications for Vista Outdoor’s 50 brands by directing and inspiring teams of designers, copywriters, photographers, and videographers.</li>
+                                    <li>Drove the visual evolution of nine brands, including defining the strategy and project goals, hands-on design of concepts, research and reporting on implementation costs, leading presentations to executive leadership, and designing brand guidelines.</li>
+                                </ul>
+                            </div>
+
+                            <div class="spacer-9">
+                                <p><strong>DEG<br>
+                                CREATIVE DIRECTOR</strong><br>
+                                April 2015 – October 2016</p>
+                                <ul>
+                                    <li>Directed a team of on-site and remote interactive and UX designers, art directors, copywriters, and videographers.</li>
+                                    <li>Secured new business – including two of the biggest accounts in the history of the agency – by leading creative and creative strategy for new business pitches.</li>
+                                    <li>Overhauled the agency’s brand by working directly with the CEO and personally executing hands-on design of print and digital sales and marketing materials.</li>
+                                </ul>
+                            </div>
+
+                            <div class="spacer-9">
+                                <p><strong>GARMIN<br>
+                                CREATIVE DIRECTOR</strong><br>
+                                May 2012 – April 2015</p>
+                                <ul>
+                                    <li>Directed a team of 50 cross-functional creative pros, who concepted and created  Garmin’s integrated communications, including advertising campaigns, videos, product packaging, in-store merchandising, events, and the design, content, and UX, of Garmin.com.</li>
+                                    <li>Drove a 258% year-over-year increase in sales from smartphones by leading the design and development of Garmin's mobile-optimized ecommerce website in 10 countries.</li>
+                                    <li>Worked with stakeholders in marketing, sales, and engineering  to improved the quality, timeliness, and impact of 60 – 80 annual product launch campaigns.</li>
+                                </ul>
+                                <div class="spacer-5">
+                                    <p><strong>WEB &amp; DIGITAL CREATIVE DIRECTOR</strong><br>
+                                    January 2008 – May 2012</p>
+                                    <ul>
+                                        <li>Directed Garmin’s web, interactive, and video teams, who created Garmin’s on-screen media, including the corporate website, ecommerce catalog, and video.</li>
+                                        <li>Founded and recruited Garmin’s Web UX team – growing it from zero to a team of more than 10 highly skilled UX designers, copywriters, researchers, and developers.</li>
+                                        <li>Hands-on design, frontend development, and performance reporting for marketing landing pages, B2C and B2B web content, and ecommerce pages and user flows.</li>
+                                    </ul>
+                                </div>
+                                <p><strong>WEB DESIGNER &amp; WEB MARKETING MANAGER</strong><br>
+                                April 2004 – January 2008</p>
+                                <ul>
+                                    <li>Coordinated the first major redesign of Garmin.com, moving from a tables-based layout managed with desktop software to a CSS-based layout controlled by a centralized CMS and integrated with an ecommerce platform.</li>
+                                    <li>Collaborated with a small team, including the Vice President of Communications, to evolve the Garmin brand, including a redesign of the logo, website, ad templates, and more.</li>
+                                </ul>
+                            </div>
+
+                            <div class="spacer-9">
+                                <h3>EDUCATION &amp; TRAINING</h3>
+                                <p><strong>The University of Utah David Eccles School of Business</strong><br>Finance for the Non-Financial Leader</p>
+                                <p><strong>UMKC, Bloch School of Management</strong><br>Senior Leadership Development Program</p>
+                                <p><strong>Johnson County Community College</strong><br>Undergraduate Degree, Communication Design</p>
+                            </div>
+
+
+
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</article>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/foot.php'); ?>
+</body>
+</html>
