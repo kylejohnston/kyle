ec12233c79c28b1417e92b23e06916a6b79fcf20
Kyle Johnston <flow14@gmail.com>
2021-01-09 13:51:08 -0600

    Header matter w/o generic metadata

---
+++ includes/head-clean.php
@@ -0,0 +1,21 @@
+	<!-- Le styles -->
+	<link href="/css/gridkit.min.css?20200525_000" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap" rel="stylesheet">
+	<link href="/css/main.min.css?20201127_000" rel="stylesheet" type="text/css">
+	<!-- <link href='//kylewjohnston.com/css/aos.css' rel='stylesheet' type='text/css'> -->
+
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+	<!-- GA - Classic -->
+	<!-- GA - Universal -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-92556-26', 'auto');
+		ga('send', 'pageview');
+	</script>
