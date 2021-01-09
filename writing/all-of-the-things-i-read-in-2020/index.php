a4f0c2ba81fa9cf6996d943b705f19286021db70
Kyle Johnston <flow14@gmail.com>
2021-01-09 13:50:22 -0600

    New post. Initial commit

---
+++ writing/all-of-the-things-i-read-in-2020/index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>All of the Things I Read in 2020 | Kyle W. Johnston</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="kyle johnston, creative director, designer, resume, contact, web designer, kansas city, garmin, flow14, vista outdoor, deg digital" />
+	<meta name="author" content="Kyle Johnston" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.kylewjohnston.com/about-kyle-johnston" rel="canonical" />
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://metatags.io/">
+	<meta property="og:title" content="All of the Things I Read in 2020">
+	<meta property="og:description" content="25 books, 316 articles.">
+	<meta property="og:image" content="https://www.kylewjohnston.com/img/books-2020/all-things-read-2020.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://metatags.io/">
+	<meta property="twitter:title" content="All of the Things I Read in 2020">
+	<meta property="twitter:description" content="25 books, 316 articles.">
+	<meta property="twitter:image" content="https://www.kylewjohnston.com/img/books-2020/all-things-read-2020.png">
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head-clean.php'); ?>
+	<style type="text/css">
+	@media (min-width: 1012px) {
+		.book-cover {
+			height:420px;
+			width:100%;
+			margin-bottom:1rem;
+			box-shadow: 0 1px 2px rgba(0,0,0,0.07),0 2px 4px rgba(0,0,0,0.07),0 4px 8px rgba(0,0,0,0.07),0 8px 16px rgba(0,0,0,0.07),0 16px 32px rgba(0,0,0,0.07),0 32px 64px rgba(0,0,0,0.07);
+			}
+		.col-11 .block{margin:2rem auto;}
+	}
+	</style>
+</head>
+
+<body>
+	<header>
+		<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/header-nav.php'); ?>
+	</header>
+	<article class="content">
+		<section class="block spacer-9--top">
+			<div class="col-1 shim"></div>
+			<div class="col-11">
+				<div class="block">
+					<div class="col-7">
+						<div class="inner">
+							<h1>All of the Things I Read in 2020</h1>
+							<p>TLDR: 25 books, 316 articles (including 21 standouts).</p>
+							<p class=""><a href="/writing/all-of-the-things-i-read-in-2019/">2019 stats</a>: 24 books, 342 articles (including 7 standouts).</p>
+							<p class="spacer-6--bottom"><a href="https://medium.com/@kylejohnston/all-of-the-things-i-read-in-2018-b7e7a5f34394">2018 stats</a>: 24 books, 527 articles (including 13 standouts).</p>
+						</div> <!-- inner -->
+					</div> <!-- col-7 -->
+				</div> <!-- block -->
+			<div class="block">
+				<div class="col-11">
+					<div class="inner spacer-6--bottom">
+						<h2>Books</h2>
+						<p><em>Listed in reading order, January 2020 &#8211; December 2020</em></p>
+					</div>
+					<div class="block">
+						<div class="col-3">
+							<a href="https://bookshop.org/books/my-sister-the-serial-killer/9780525564201"><img class="book-cover" src="/img/books-2020/my-sister-the-serial-killer.jpg" alt="My Sister, the Serial Killer"></a>
+							<h4 class="spacer-2">My Sister, the Serial Killer</h4>
+							<p>Oyinkan Braithwaite</p>
+						</div>
+						<div class="col-3">
+							<a href="https://bookshop.org/books/annihilation-9780374104092/9780374104092"><img class="book-cover" src="/img/books-2020/annihilation.jpg" alt="Annihilation"></a>
+							<h4 class="spacer-2">Annihilation</h4>
+							<p>Jeff VanderMeer</p>
+						</div>
+						<div class="col-3">
+							<a href="https://bookshop.org/books/blood-meridian-or-the-evening-redness-in-the-west/9780679728757"><img class="book-cover" src="/img/books-2020/blood-meridian.jpg" alt="Blood Meridian"></a>
+							<h4 class="spacer-2">Blood Meridian</h4>
+							<p>Cormac McCarthy</p>
+						</div>
+						<div class="col-3">
+							<a href="https://bookshop.org/books/nothing-to-see-here-9780062913463/9780062913463"><img class="book-cover" src="/img/books-2020/nothing-to-see-here.jpg" alt="Nothing to See Here"></a>
+							<h4 class="spacer-2">Nothing to See Here</h4>
+							<p>Kevin Wilson</p>
+						</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/authority-9780374104108/9780374104108"><img class="book-cover" src="/img/books-2020/authority.jpg" alt="Authority"></a>
+								<h4 class="spacer-2">Authority</h4>
+								<p>Jeff VanderMeer</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/acceptance-9780374104115/9780374104115"><img class="book-cover" src="/img/books-2020/acceptance.jpg" alt="Acceptance"></a>
+								<h4 class="spacer-2">Acceptance</h4>
+								<p>Jeff VanderMeer</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/educated-a-memoir-9781987146127/9780399590504"><img class="book-cover" src="/img/books-2020/educated.jpg" alt="Educated"></a>
+								<h4 class="spacer-2">Educated</h4>
+								<p>Tara Westover</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/hollow-kingdom/9781538745830"><img class="book-cover" src="/img/books-2020/hollow-kingdom.jpg" alt="Hollow Kingdom"></a>
+								<h4 class="spacer-2">Hollow Kingdom</h4>
+								<p>Kira Jane Buxton</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/say-nothing-a-true-story-of-murder-and-memory-in-northern-ireland/9780307279286"><img class="book-cover" src="/img/books-2020/say-nothing.jpg" alt="Say Nothing"></a>
+								<h4 class="spacer-2">Say Nothing: A True Story of Murder and Memory in Northern Ireland</h4>
+								<p>Patrick Radden Keefe</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/the-topeka-school/9780374277789"><img class="book-cover" src="/img/books-2020/the-topeka-school.jpg" alt="The Topeka School"></a>
+								<h4 class="spacer-2">The Topeka School</h4>
+								<p>Ben Lerner</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/race-matters-25th-anniversary-with-a-new-introduction/9780807008836"><img class="book-cover" src="/img/books-2020/race-matters.jpg" alt="Race Matters"></a>
+								<h4 class="spacer-2">Race Matters</h4>
+								<p>Cornel West</p>
+							</div>
+							<div class="col-3">
+								<a href="#"><img class="book-cover" src="/img/books-2020/super-sad-true-love-story.jpg" alt="Super Sad True Love Story"></a>
+								<h4 class="spacer-2">Super Sad True Love Story</h4>
+								<p>Gary Shteyngart</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/super-sad-true-love-story-9780812977868/9780812977868"><img class="book-cover" src="/img/books-2020/how-to-absurd-scientific-advice.jpg" alt="How To: Absurd Scientific Advice for Common Real-World Problems"></a>
+								<h4 class="spacer-2">How To: Absurd Scientific Advice for Common Real-World Problems</h4>
+								<p>Randall Munroe</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/the-fire-next-time/9780679744726"><img class="book-cover" src="/img/books-2020/the-fire-next-time.jpg" alt="The Fire Next Time"></a>
+								<h4 class="spacer-2">The Fire Next Time</h4>
+								<p>James Baldwin</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/when-things-fall-apart-heart-advice-for-difficult-times-9781611803433/9781611803433"><img class="book-cover" src="/img/books-2020/when-things-fall-apart.jpg" alt="When Things Fall Apart: Heart Advice for Difficult Times"></a>
+								<h4 class="spacer-2">When Things Fall Apart: Heart Advice for Difficult Times</h4>
+								<p>Pema Chödrön</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/gulp-adventures-on-the-alimentary-canal/9780393348743"><img class="book-cover" src="/img/books-2020/gulp.jpg" alt="Gulp: Adventures on the Alimentary Canal"></a>
+								<h4 class="spacer-2">Gulp: Adventures on the Alimentary Canal</h4>
+								<p>Mary Roach</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/start-where-you-are-a-guide-to-compassionate-living/9781611805970"><img class="book-cover" src="/img/books-2020/start-where-you-are.jpg" alt="Start Where You Are"></a>
+								<h4 class="spacer-2">Start Where You Are</h4>
+								<p>Pema Chödrön</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/a-grief-observed-9780060652388/9780060652388"><img class="book-cover" src="/img/books-2020/a-grief-observed.jpg" alt="A Grief Observed"></a>
+								<h4 class="spacer-2">A Grief Observed</h4>
+								<p>C.S. Lewis</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/talking-to-strangers-what-we-should-know-about-the-people-we-don-t-know/9780316478526"><img class="book-cover" src="/img/books-2020/talking-to-strangers.jpg" alt="Talking to Strangers"></a>
+								<h4 class="spacer-2">Talking to Strangers</h4>
+								<p>Malcolm Gladwell</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/ten-arguments-for-deleting-your-social-media-accounts-right-now/9781250239082"><img class="book-cover" src="/img/books-2020/deleting-your-social-media.jpg" alt="Ten Arguments for Deleting Your Social Media Accounts Right Now"></a>
+								<h4 class="spacer-2">Ten Arguments for Deleting Your Social Media Accounts Right Now</h4>
+								<p>Jaron Lanier</p>
+							</div>
+							<div class="col-3">
+								<a href="https://www.facebook.com/business/buildbrilliantbrands"><img class="book-cover" src="/img/books-2020/build-brilliant-brands.jpg" alt="Build Brilliant Brands"></a>
+								<h4 class="spacer-2">Build Brilliant Brands</h4>
+								<p>Facebook EMEA Client Council</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/blindsight-9781250237484/9781250237484"><img class="book-cover" src="/img/books-2020/blindsight.jpg" alt="Blindsight"></a>
+								<h4 class="spacer-2">Blindsight</h4>
+								<p>Peter Watts</p>
+							</div>
+							<div class="col-3">
+								<a href="https://bookshop.org/books/how-to-be-an-artist/9780593086469"><img class="book-cover" src="/img/books-2020/how-to-be-an-artist.jpg" alt="How to Be an Artist"></a>
+								<h4 class="spacer-2">How to Be an Artist</h4>
+								<p>Jerry Saltz</p>
+							</div>
+							<div class="col-3">
+							<a href="https://bookshop.org/books/creative-selection-inside-apple-s-design-process-during-the-golden-age-of-steve-jobs/9781250203410"><img class="book-cover" src="/img/books-2020/creative-selection.jpg" alt="Creative Selection"></a>
+								<h4 class="spacer-2">Creative Selection: Inside Apple’s Design Process During the Golden Age of Steve Jobs</h4>
+								<p>Ken Kocienda</p>
+							</div>
+							<div class="col-3">
+							<a href="https://bookshop.org/books/writing-down-the-bones-freeing-the-writer-within/9781611803082"><img class="book-cover" src="/img/books-2020/writing-down-the-bones.jpg" alt="Writing Down the Bones"></a>
+								<h4 class="spacer-2">Writing Down the Bones</h4>
+								<p>Natalie Goldberg</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="block spacer-9--top">
+			<div class="col-1 shim"></div>
+			<div class="col-11">
+					<div class="col-11">
+						<div class="inner">
+							<h2>Articles &#8211; the standouts</h2>
+							<p><em>Things that I ⭐️’d because I especially enjoyed them, starting with the most recently read.</em></p>
+							<ol class="spacer-9--bottom">
+								<li><a href="https://www.lesswrong.com/posts/7hFeMWC6Y5eaSixbD/100-tips-for-a-better-life">&ldquo;100 Tips for a Better Life&rdquo;</a></li>
+								<li><a href="https://library.gv.com/guide-to-design-critique-86ebf499bed5">&ldquo;GV Guide to Design Critique&rdquo;</a> Braden Kowitz</li>
+								<li><a href="https://www.theatlantic.com/ideas/archive/2020/11/why-obama-fears-for-our-democracy/617087/">&ldquo;Why Obama Fears for Our Democracy&rdquo;</a> Jeffrey Goldberg, The Atlantic</li>
+								<li><a href="https://www.creativeboom.com/features/frank-chimero/">&ldquo;Frank Chimero on causing &lsquo;good trouble&rsquo; and re-imagining the status quo to combat achievement culture&rdquo;</a> Katy Cowan, Creative Boom</li>
+								<li><a href="https://www.wearecollins.com/ideas/no-artist-is-pleased/">&ldquo;No artist is pleased&rdquo;</a> Paul Jun, Collins</li>
+								<li><a href="https://www.nytimes.com/2020/07/30/opinion/john-lewis-civil-rights-america.html">&ldquo;Together, You Can Redeem the Soul of Our Nation&rdquo;</a> John Lewis, The New York Times</li>
+								<li><a href="https://www.martinweigel.org/blog/hope-is-an-axe">&ldquo;Hope is an axe&rdquo;</a> Martin Weigel</li>
+								<li><a href="https://automattic.design/2020/07/09/sticking-to-it/">&ldquo;Sticking To It&rdquo;</a> Automattic Design</li>
+								<li><a href="https://ethanmarcotte.com/wrote/the-design-systems-between-us/">&ldquo;The design systems between us&rdquo;</a> Ethan Marcotte</li>
+								<li><a href="https://medium.com/@BarackObama/how-to-make-this-moment-the-turning-point-for-real-change-9fa209806067">&ldquo;How to Make this Moment the Turning Point for Real Change&rdquo;</a> Barack Obama</li>
+								<li><a href="https://www.warc.com/newsandopinion/opinion/100-things-a-planner-should-know/3591">&ldquo;100 Things a Planner Should Know&rdquo;</a> Charles Wigley, BBH</li>
+								<li><a href="https://ricklepage.com/essays/do-the-work/">&ldquo;Do. The. Work.&rdquo;</a> Rick LePage</li>
+								<li><a href="https://kk.org/thetechnium/68-bits-of-unsolicited-advice/">&ldquo;68 Bits of Unsolicited Advice&rdquo;</a> Kevin Kelly</li>
+								<li><a href="https://www.nytimes.com/2020/03/24/magazine/coronavirus-family.html">&ldquo;What I Learned When My Husband Got Sick With Coronavirus&rdquo;</a> Jessica Lustig, The New York Times</li>
+								<li><a href="https://www.bakadesuyo.com/2019/12/change-someones-mind/">&ldquo;This Is How To Change Someone’s Mind: 6 Secrets From Research&rdquo;</a> Eric Barker</li>
+								<li><a href="https://www.robinsloan.com/notes/home-cooked-app/">&ldquo;An app can be a home-cooked meal&rdquo;</a> Robin Sloan</li>
+								<li><a href="https://uncertaintymindset.substack.com/p/15-maintenance-by-design">&ldquo;Maintenance by design&rdquo;</a> Vaughn Tan</li>
+								<li><a href="https://www.theatlantic.com/magazine/archive/2020/03/the-2020-disinformation-war/605530/">&ldquo;The 2020 Election Will Be a War of Disinformation&rdquo;</a>McKay Coppins, The Atlantic</li>
+								<li><a href="https://medium.com/@faris/modern-modes-of-persuasion-bb35fd6562b5">&ldquo;Modern Modes of Persuasion&rdquo;</a> Faris Jakob</li>
+								<li><a href="https://www.martinweigel.org/blog/km1vsiqn3gxcjh6vxdlonklylj1r08">&ldquo;All watched by corporations of loving grace? It’s time we punctured the feverish toxic dream.&rdquo;</a> Martin Weigel</li>
+								<li><a href="https://postlight.com/trackchanges/modern-javascript-for-ancient-web-developers">&ldquo;Modern JavaScript for Ancient Web Developers&rdquo;</a> Gina Trapani, Postlight</li>
+							</ol>
+						</div> <!-- inner -->
+					</div> <!-- col-7 -->
+			</div>
+		</section>
+	</article>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/foot.php'); ?>
+</body>
+</html>
