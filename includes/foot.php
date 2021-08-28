5c1ab6d8662342ef1841fae04f4c354a92e00ce5
Kyle Johnston <flow14@gmail.com>
2021-08-28 15:53:22 -0500

    Housecleaning. Removed old JS

--- includes/foot.php
+++ includes/foot.php
@@ -1,21 +1,9 @@
-<!-- Fun with JavaScript - - - - - - - - - - -->
-	<!-- <script src="//kylewjohnston.com/js/aos.js"></script>
-    <script type="text/javascript">
-      AOS.init({
-        duration: 700,
-        easing: 'ease-in-out-back'
-      });
-    </script> -->
-    <!-- <script async src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> -->
-    <script>window.jQuery || document.write('<script src="//flow14.com/js/jquery.js">\x3C/script>')</script>
-    <script async src="//kylewjohnston.com/js/jquery.flexslider-min.js"></script>
-
 <!-- Analytics - - - - - - - - - - -->
 <!-- statcounter -->
 <script type="text/javascript">
-var sc_project=10324514; 
-var sc_invisible=1; 
-var sc_security="42057f08"; 
+var sc_project=10324514;
+var sc_invisible=1;
+var sc_security="42057f08";
 var scJsHost = (("https:" == document.location.protocol) ?
 "https://secure." : "http://www.");
 document.write("<sc"+"ript type='text/javascript' src='" +
